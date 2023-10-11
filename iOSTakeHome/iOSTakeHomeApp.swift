47401001b738263af0433852f55ea5776a551b48
Luis Roberto Gutiérrez Carbajal <lrobgc.kp@gmail.com>
2023-10-11 17:29:14 -0600

    [Feature][ROB] Project created and added to the repository

---
+++ iOSTakeHome/iOSTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSTakeHomeApp.swift
+//  iOSTakeHome
+//
+//  Created by Luis Roberto Gutiérrez Carbajal on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct iOSTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
